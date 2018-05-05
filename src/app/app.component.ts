20b4b19d7397f13b37bd3781331e249ccf629783
Tyler-Maclachlan <tmaclach@student.wethinkcode.co.za>
2018-05-05 10:10:14 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit()  {
+    $('#welcome1').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", () => {
+      $('#welcome2').addClass('welcome-animate');
+    });
+
+    $('#welcome2').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", () => {
+      $('#welcome').animate({
+        opacity: 0
+      }, {
+        duration: 1000,
+        complete: ()  =>  {
+          $('#welcome').remove();
+        }
+      })
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { jquery } from 'jquery';
+import { bootstrap } from 'bootstrap';
 
 import { AppComponent } from './app.component';
 
